2ec4955dd506975810e8836b8e7dfa859d29f9b9
graceunhye <9rac.eun@gmail.com>
2021-01-19 17:48:39 +0900

    update

--- TeamB/src/main/java/com/skylibrary/controller/HomeController.java
+++ TeamB/src/main/java/com/skylibrary/controller/HomeController.java
@@ -6,8 +6,6 @@ import java.util.Date;*/
 import java.util.Locale;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;*/
